246b4f54e11ed32de2397906c3dd45759ad593c2
Martin Zackrisson <martin.zackrisson@fripost.org>
2025-11-17 11:18:29 +0100

    Create a new movement planner

--- scripts/gridded_level/entities/grid_entity.gd
+++ scripts/gridded_level/entities/grid_entity.gd
@@ -58,7 +58,12 @@ func remove_cinematic_cause(cause: Node) -> void:
 @export var transportation_abilities: TransportationMode
 @export var transportation_mode: TransportationMode
 
-@export var can_jump_off_walls: bool
+@export var can_jump_off_floor: bool:
+    get():
+        return can_jump_off_floor || can_jump_off_all
+
+@export var can_jump_off_all: bool
+@export var orient_with_gravity_in_air: bool = true
 
 @export var planner: MovementPlanner
 
